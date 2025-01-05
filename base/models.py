b9d8277f2fad00bf7122af71dd1769fa6d37b1a2
Robert Potemkin <rp00549@rvt.lv>
2025-01-05 22:06:43 +0200

    Add Topic and Message models

--- base/models.py
+++ base/models.py
@@ -1,17 +1,34 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name[0:50]
+
 
 class Room(models.Model):
-    id = models.UUIDField(primary_key=True, unique=True, editable=False, default=uuid.uuid4)
-    # host =
-    # topic =
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    host = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    # participants =
+    description = models.TextField(blank=True, null=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]

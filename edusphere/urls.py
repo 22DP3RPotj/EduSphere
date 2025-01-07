b4e42dae5459a716d737c00fcc305493c14a7e61
Robert Potemkin <rp00549@rvt.lv>
2025-01-07 21:50:37 +0200

    Add exception handling templates

--- edusphere/urls.py
+++ edusphere/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for EduSphere project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,3 +5,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
 ]
+
+handler400 = "base.views.error_views.custom_400"
+handler403 = "base.views.error_views.custom_403"
+handler404 = "base.views.error_views.custom_404"
+handler500 = "base.views.error_views.custom_500"

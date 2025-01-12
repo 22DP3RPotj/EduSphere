7ee7e4fc20271218276db859df6cca6f74c10b6c
Robert Potemkin <rp00549@rvt.lv>
2025-01-12 15:55:37 +0200

    Organize js code

--- static/api/js/delete.js
+++ static/api/js/delete.js
@@ -1,3 +1,5 @@
+import { getCookie } from './utils.js';
+
 async function confirmDelete(url) {
     const result = await Swal.fire({
         title: '<h3>Are you sure?</h3>',
@@ -39,58 +41,6 @@ document.querySelectorAll('.delete-button').forEach(button => {
     button.addEventListener('click', (event) => {
         const url = event.target.dataset.url;
         confirmDelete(url);
-    });
-});
-
-
-
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    const csrftoken = getCookie('csrftoken');
-    const messageInput = document.getElementById('chat-message-input');
-    const messageSubmit = document.getElementById('chat-message-submit');
-
-    function sendMessage() {
-        const message = messageInput.value.trim();
-    
-        if (!message) {
-            return;
-        }
-    
-        fetch('', {
-            method: 'POST',
-            headers: {
-                'X-CSRFToken': csrftoken,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ 'body': message }),
-        })
-        .then(() => location.reload())
-        .catch((err) => console.error(err));
-    }
-    
-
-    messageSubmit.addEventListener('click', sendMessage);
-
-    messageInput.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-            event.preventDefault();
-            sendMessage();
-        }
     });
 });
 
---
+++ static/api/js/messages.js
@@ -0,0 +1,37 @@
+import { getCookie } from './utils.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const csrftoken = getCookie('csrftoken');
+    const messageInput = document.getElementById('chat-message-input');
+    const messageSubmit = document.getElementById('chat-message-submit');
+
+    function sendMessage() {
+        const message = messageInput.value.trim();
+    
+        if (!message) {
+            return;
+        }
+    
+        fetch('', {
+            method: 'POST',
+            headers: {
+                'X-CSRFToken': csrftoken,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 'body': message }),
+        })
+        .then(() => location.reload())
+        .catch((err) => console.error(err));
+    }
+    
+
+    messageSubmit.addEventListener('click', sendMessage);
+
+    messageInput.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            sendMessage();
+        }
+    });
+});
+

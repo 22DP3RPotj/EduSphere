7cee937ff7fad7748f29b28e27429945184a29a0
Robert Potemkin <rp00549@rvt.lv>
2025-06-01 14:23:32 +0300

    Migrate to pnpm

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,6 @@
 
 source ./scripts/setup.sh
 
-npm --prefix frontend run build
+pnpm --prefix frontend run build
 
 uvicorn backend.config.asgi:application --host 127.0.0.1 --port 8000 --lifespan=off --reload 

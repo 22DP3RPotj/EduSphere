297abfd0f39c3ec8f3d4a1a2d54d698b4f6f8898
Robert Potemkin <rp00549@rvt.lv>
2026-01-07 19:23:34 +0200

    Update CI/CD

--- backend/infra/views/info.py
+++ backend/infra/views/info.py
@@ -8,8 +8,3 @@ def version(request: HttpRequest) -> JsonResponse:
     return JsonResponse(
         {"version": settings.APP_VERSION, "commit": settings.GIT_SHA}, status=200
     )
-
-
-# TODO: inject
-# GIT_SHA=$(git rev-parse --short HEAD)
-# APP_VERSION=$(git describe --tags --dirty --always)

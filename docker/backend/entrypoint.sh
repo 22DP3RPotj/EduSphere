2ad46c10b7df89a64ba2175d0aec0400bdf01f7e
Robert Potemkin <rp00549@rvt.lv>
2026-03-04 19:07:07 +0200

    Minor improvements

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -e
+
 until pg_isready -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do sleep 1; done
 
 if ! python manage.py migrate --check; then
     python manage.py migrate --noinput
 fi
 
-exec "$@"
+chown -R appuser:appuser /app/staticfiles /app/media
+
+exec gosu appuser "$@"

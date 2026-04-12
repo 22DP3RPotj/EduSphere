3c930b6b7beaa88f4c680bd2a0933d6e0c5270a0
Robert Potemkin <rp00549@rvt.lv>
2026-04-12 12:01:53 +0300

    Update globals

--- frontend/globals.d.ts
+++ frontend/globals.d.ts
@@ -1,2 +1,9 @@
+// Constants
 declare const __API_URL__: string;
 declare const __WS_URL__: string;
+
+// Modules
+declare module "*.css";
+declare module "*.scss";
+declare module "*.sass";
+declare module "*.less";
